d274c99448f7969731c94ea90900182165661dd0
sysboss <sysboss@mail.ru>
2016-08-03 12:05:48 +0300

    visits counter implementation

--- roles/bamboo/files/bamboo-app/bamboo.js
+++ roles/bamboo/files/bamboo-app/bamboo.js
@@ -1,11 +1,41 @@
-var http = require('http');
-var config = require('./config.json');
-var dispatcher = require('httpdispatcher');
+/*
+    BigService Bamboo App
+*/
+
+var http       = require('http'),
+    config     = require('./config.json'),
+    dispatcher = require('httpdispatcher'),
+    redis      = require("redis").createClient();
+
+// connect to Redis
+redis.on('connect', function() {
+    console.log('Connected to Redis!');
+
+    // init visits counter
+    redis.exists('counter', function(err, reply) {
+        if (reply !== 1) {
+            console.log('Initializing visits counter');
+            redis.set('counter', 0);
+        }
+    });
+});
 
 function handleRequest(request, response){
     try {
         console.log("Requested URL: " + request.url);
-        dispatcher.dispatch(request, response);
+
+        redis.incr('counter', function(err, reply) {
+            console.log('COUNTER: ' + reply);
+            var counter = "\rGet requests: " + reply,
+                _write  = response.write;
+
+            // inject counter
+            response.write = function (data) {
+                _write.call(response, data.toString() + counter);
+            }
+
+            dispatcher.dispatch(request, response);
+        });
     } catch(err) {
         console.log(err);
     }
@@ -17,8 +47,8 @@ dispatcher.onGet("/", function(req, res) {
 });
 
 dispatcher.onError(function(req, res) {
-        res.writeHead(404);
-        res.end("404 - Page Does not exists");
+    res.writeHead(404);
+    res.end("404 - Page Does not exists");
 });
 
 http.createServer(handleRequest).listen(config.port, function(){
